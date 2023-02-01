1d4a3669283276574aaaa63a3b0862fdb73fd0e9
Asfandyar <asfandm17@gmail.com>
2023-02-01 09:02:02 +0500

    some changes

--- src/components/sendMail/SendMail.jsx
+++ src/components/sendMail/SendMail.jsx
@@ -2,24 +2,52 @@ import React from "react";
 import "./sendMail.css";
 import CloseIcon from "@mui/icons-material/Close";
 import { Button } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 const SendMail = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <main className="sendMail">
       <section className="sendMail_header">
         <h3>New Message</h3>
         <CloseIcon className="close_sendMail" />
       </section>
-      <form>
-        <input type="text" placeholder="To" />
-        <input type="text" placeholder="Subject" />
-        <input type="text" placeholder="Message" className="sendMail_message" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="To"
+          {...register("to", { required: true })}
+        />
+        {errors.to && <p>This field is required!</p>}
+        <input
+          type="text"
+          placeholder="Subject"
+          {...register("subject", { required: true })}
+        />
+        {errors.subject && <p>This field is required!</p>}
+        <input
+          type="text"
+          name="message"
+          placeholder="Message"
+          className="sendMail_message"
+          {...register("message", { required: true })}
+        />
+        {errors.message && <p>This field is required!</p>}
         <div className="sendMail_option">
           <Button
             variant="contained"
-            type="submit"
             size="small"
             sx={{ margin: "10px", textTransform: "capitalize !important" }}
+            type="submit"
           >
             Send
           </Button>
